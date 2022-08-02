f5ef552abeeb950d0834f3e9a195b56cc3a95ce0
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-08-02 12:08:40 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,68 @@
+let twoRaiseTo2 = Math.pow(2, 2)
+console.log("2 raised to 2 is:", twoRaiseTo2)
+let twoRaiseTo3 = Math.pow(2, 3)
+console.log("2 raised to 3 is:", twoRaiseTo3)
+let twoRaiseTo6 = Math.pow(6, 2)
+console.log("2 raised to 6 is:", twoRaiseTo6)
+
+
+function square() {
+console.log(2**2)
+}
+ for (let count = 1; count <=10;count++){
+    square()
+ }
+
+ function printname() { 
+    console.log("ogugua")
+ }
+ printname()
+
+ function multiply(){ 
+    let number1 = 2
+    let number2 = 4
+    
+    let product = number1 * number2
+
+    let message = '2 times 4 is ' + product
+    console.log(message)
+ }
+ multiply()
+
+
+ function division(){ 
+    let number1 = 4
+    let number2 = 2
+    
+    let divide = number1 / number2
+
+    let message = '4 divide 2 is ' + divide
+    console.log(message)
+ }
+ division()
+
+
+ function subtraction(){ 
+    let number1 = 8
+    let number2 = 2
+    
+    let subtraction = number1 - number2
+
+    let message = '8 minus 2 is ' + subtraction
+    console.log(message)
+ }
+subtraction()
+
+
+
+
+function addition(){ 
+    let number1 = 8
+    let number2 = 2
+    
+    let addition = number1 + number2
+
+    let message = '8 added to 2 is ' +  addition
+    console.log(message)
+ }
+ addition()
